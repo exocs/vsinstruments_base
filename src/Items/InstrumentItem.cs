f7b6704e7d88c930752d8abb43a99ea486b78ab2
Aleš Stupka <41081516+exocs@users.noreply.github.com>
2025-10-08 00:55:48 +0200

    Unused include

--- src/Items/InstrumentItem.cs
+++ src/Items/InstrumentItem.cs
@@ -4,7 +4,6 @@ using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.MathTools;
-using Vintagestory.API.Util;
 using Midi;
 using Instruments.GUI;
 using Instruments.Network.Packets;
